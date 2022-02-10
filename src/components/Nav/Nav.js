6664db88cdca56430dacbca17e00c98837ec848d
Wilfred Onyinye <ajolaiya@sidmach.com>
2022-02-10 19:09:06 +0100

    Initial Commit

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from '../../images/logo.svg';
+import cartIcon from '../../images/cartIcon.svg';
+import '../Nav/nav.css';
+
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <>
+        <nav className='navbar'>
+          <div className='d-flex justify-content-between'>
+            <div>
+              <ul className='list-style d-flex'>
+                <li className='space-between'>WOMEN</li>
+                <li className='space-between'>MEN</li>
+                <li className='space-between'>KIDS</li>
+              </ul>
+            </div>
+            <div>
+              <img src={logo} alt='logo' />
+            </div>
+            <div className='d-flex justify-content-center align-items-center'>
+              <select className='space-between'>
+                <option value="1">$ USD</option>
+                <option value="2">&pound; EUR</option>
+                <option value="3"># JPY</option>
+              </select>
+              <img className='space-between' src={cartIcon} alt='cartIcon' />
+            </div>
+          </div>
+        </nav>
+
+      </>
+    );
+  }
+};
